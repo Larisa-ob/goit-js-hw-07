9b867cf77d2e7978f3a5a9e7943d0a9d01a9268e
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2020-11-09 23:17:56 +0200

    Первая часть задачи1

--- js/task-7-1.js
+++ js/task-7-1.js
@@ -1,14 +1,2 @@
-const ingredients = [
-  "Картошка",
-  "Грибы",
-  "Чеснок",
-  "Помидоры",
-  "Зелень",
-  "Приправы",
-];
-const getUserNames = users.map(function (user) {
-  return user.name;
-});
-
-console.log(getUserNames);
-// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+const titleItemsRef = document.querySelectorAll("#categories .item");
+console.log(`В списке ${titleItemsRef.length} категории`);
