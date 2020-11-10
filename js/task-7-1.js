2e49d19dafa76e11ce7ef4dbb9a4c62b5c18f8d0
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2020-11-10 23:18:18 +0200

    Решение 1 задачи

--- js/task-7-1.js
+++ js/task-7-1.js
@@ -1,2 +1,14 @@
 const titleItemsRef = document.querySelectorAll("#categories .item");
 console.log(`В списке ${titleItemsRef.length} категории`);
+const textItemsRef = document.querySelectorAll("h2");
+const array = [...textItemsRef];
+const textItems = (array) =>
+  array.forEach((element) => {
+    return (
+      console.log(`.Категория ${element.textContent}`),
+      console.log(
+        `.Количество элементов ${element.nextSibling.nextSibling.childElementCount}`
+      )
+    );
+  });
+textItems(array);
