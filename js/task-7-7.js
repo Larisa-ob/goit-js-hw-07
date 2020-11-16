2fb627da083891911192aeb59d60885a12b80e02
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2020-11-16 20:42:35 +0200

    Домашнее задание 7 JS

--- js/task-7-7.js
+++ js/task-7-7.js
@@ -1,10 +1,8 @@
 const inputRef = document.querySelector("#font-size-control");
-const nameLabelRef = document.querySelector('span[id="text"]');
+const textRef = document.querySelector("#text");
 
-inputRef.addEventListener("input", (e) => {
-  console.log(e);
-});
-inputRef.addEventListener("mouseenter", (e) => {
-  e.target.maxRange = 100;
-  console.log(e.target);
-});
+inputRef.addEventListener("input", handleFontSizeText);
+
+function handleFontSizeText(event) {
+  textRef.style.fontSize = event.currentTarget.value + "px";
+}
