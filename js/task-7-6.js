ee1c54a68391d99ad4619734168821765d865d1d
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2021-01-13 00:38:35 +0200

    Добавлена 8 задача

--- js/task-7-6.js
+++ js/task-7-6.js
@@ -1,14 +1,15 @@
 const inputTextRef = document.querySelector('#validation-input');
+
 inputTextRef.addEventListener('input', e => {
     e.preventDefault();
-    const inputdataLength = e.target.attributes['data-length'].nodeValue;
 });
 /* При потере фокуса меняем цвет бордюра по условию*/
 inputTextRef.addEventListener('blur', event => {
     event.preventDefault();
     const inputLength = event.target.value.length;
+    console.log(inputLength);
     const inputdataLength = event.target.attributes['data-length'].nodeValue;
-    if (inputLength === inputdataLength) {
+    if (inputLength == inputdataLength) {
         inputTextRef.classList.remove('invalid');
         inputTextRef.classList.add('valid');
     } else {
--- js/task-7-8.js
+++ js/task-7-8.js
@@ -1,12 +1,42 @@
-const divcontrolRef = document.querySelector("#controls");
-const inputRef = document.querySelector("js-input");
+const inputNumber = document.querySelector('input[type="number"]');
 const buttonRender = document.querySelector('button[data-action="render"]');
 const buttonDestroy = document.querySelector('button[data-action="destroy"]');
-const inputNumber = inputRef.addEventListener("input", createBoxes);
-
-function createBoxes(amount) {
-  console.log(event);
-  amount = inputNumber.textContant.value;
-  console.log(amount);
+const boxesDiv = document.querySelector('div#boxes');
+const boxesArray = [];
+const startWidth = 30;
+const startHeight = 30;
+let amount = 0;
+function genColor() {
+    const r = function () {
+        return Math.floor(Math.random() * 256);
+    };
+    return 'rgb(' + r() + ',' + r() + ',' + r() + ')';
 }
-createBoxes();
+const createBoxes = amount => {
+    amount = Number(inputNumber.value);
+    for (let i = 0; i < amount; i += 1) {
+        const newdiv = document.createElement('div');
+        newdiv.style.width = `${startWidth + 10 * i}px`;
+        newdiv.style.height = `${startHeight + 10 * i}px`;
+        newdiv.style.backgroundColor = genColor();
+        boxesArray.push(newdiv);
+    }
+
+    boxesDiv.append(...boxesArray);
+};
+
+const destroyBoxes = () => {
+    const boxesCurrent = boxesDiv.querySelectorAll('div');
+    for (const boxesCurrentItem of boxesCurrent) {
+        boxesDiv.removeChild(boxesCurrentItem);
+    }
+    inputNumber.value = null;
+    clearArray(boxesArray);
+};
+function clearArray(array) {
+    while (array.length) {
+        array.pop();
+    }
+}
+buttonRender.addEventListener('click', createBoxes);
+buttonDestroy.addEventListener('click', destroyBoxes);
